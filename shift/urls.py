e40285a3570f123f33ba03117f0b501b2c195e32
yucho115 <8villageisfun16@gmail.com>
2025-11-24 01:40:53 +0900

    招待メールの送信

--- shift/urls.py
+++ shift/urls.py
@@ -6,4 +6,5 @@ app_name = 'shift'
 urlpatterns = [
     path('',views.IndexView.as_view(),name="index"),
     path('inquiry/',views.InquiryView.as_view(),name="inquiry"),
+    path('invite/',views.InviteView.as_view(),name="invite"),
 ]
--- shift/views.py
+++ shift/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 import logging
 from django.views import generic
-from .forms import InquiryForm
+from .forms import InquiryForm, InviteForm
 from django.contrib import messages
 from django.urls import reverse_lazy
+from .models import Username
 
 logger = logging.getLogger(__name__)
 
@@ -21,4 +22,16 @@ class InquiryView(generic.FormView):
         form.send_email()
         messages.success(self.request, 'メッセージを送信しました。')
         logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
+    
+class InviteView(generic.CreateView):
+    model = Username
+    template_name = 'invite.html'
+    form_class = InviteForm
+    success_url = reverse_lazy('shift:invite')
+
+    def form_valid(self, form):
+        form.send_email()
+        form.save()
+        messages.success(self.request,'招待メッセージを送信しました。')
         return super().form_valid(form)
